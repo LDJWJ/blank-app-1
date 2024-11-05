85559f4b22de478917347f271593081ee5a2a5cd
LDJWJ <frontierall@naver.com>
2024-11-05 10:33:16 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My toto app")
+st.title("🎈 My 이로운님 app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
